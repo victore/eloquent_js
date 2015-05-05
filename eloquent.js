f980e023ba6512151eb118141aaf4a6b293cc907
Victor Guadarrama <victor.guadarrama3@gmail.com>
2015-05-04 19:59:02 -0500

    eloquent js

---
+++ eloquent.js
@@ -0,0 +1,4 @@
+var cadena = "And my heart glows bright red under my filmy, translucent skin and they have to administer 10cc of JavaScript to get me to come back. (I respond well to toxins in the blood.) Man, that stuff will kick the peaches right out your gills!";
+document.write(cadena);
+
+console.log(Math.max(2, 4));
